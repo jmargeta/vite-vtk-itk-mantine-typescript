cbc2faa6f9906040f6a567ce62d6d37871fb2f56
Jan Margeta <jmargeta@gmail.com>
2022-10-20 04:22:06 +0200

    Add Vtk demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { MantineProvider, Text } from '@mantine/core';
 import Drop from './components/Drop';
-
+import VTKTest from './components/VTKTest';
 export default function App() {
   return (
     <MantineProvider withGlobalStyles withNormalizeCSS>
-      <Text>Welcome to Mantine!</Text>
-      <Drop />
+      {/* <Text>Welcome to Mantine!</Text> */}
+      {/* <Drop /> */}
+      <VTKTest />
+      <VTKTest />
     </MantineProvider>
   );
 }
